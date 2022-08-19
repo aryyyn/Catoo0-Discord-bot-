aaeb676092819d8989e3d19ca51bb948df69e649
Aryan <61741148+beachchicken@users.noreply.github.com>
2022-08-19 08:38:22 +0545

    Update catoo0.py

--- catoo0.py
+++ catoo0.py
@@ -13,28 +13,9 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client(intents=discord.Intents.all())
 
-test_list = ['pocky x kadota',
+test_list = ['aryn#5511',
 'ok and?',
-'go back to roblox, mr lee',
-'aryn gets more bitches than you',
-'pocky ko chak ma jane ho?',
-'listening to nitrix clapping eldiablos butt',
-'bro shut up, im playing with aryns huge pp',
-'bro shut up, im playing with shrikants small pp',
-'bro shut up, im playing with kadotas fat pp',
-'so today game night or movie night?',
-'timi gay ho',
-'your mom my aunt',
-'maths sikne',
-'sutna dey na lado',
-'bahun mug, ja gaera khutta ko jhyau katera aija paila',
-'kolle sodhyo mug talai?',
-'padhna ja mug',
-'https://discord.gg/cvNa9XTbD9',
-'sathi chaina mug tero?',
-'ja gaera aryn ko lado chus',
-':evil:',
-'when nitro, ako?',
+'random message here'.
 
 
 
@@ -75,46 +56,13 @@ async def on_message(message):
 
 
     elif msg.lower() == 'samosa dai':
-         await message.channel.send('gayo aryn ko chak ma')
+         await message.channel.send('gayo samosa khana')
          return
          print("message sent")
    
    
     
-    elif msg.lower() == 'pocky':
-     await message.channel.send('gayo akatora ko chak ma')
-
-    elif msg.lower() == 'ako':
-     await message.channel.send('gayo original dai ko chak ma')
-
-    
-    elif msg.lower() == 'shrikant':
-     await message.channel.send('gayo pocky ko chak ma')
-     
-     
-    
-    elif msg.lower() == 'kadota':
-     await message.channel.send('gayo samosa dai ko chak ma')
-
-
-
-    elif msg.lower() == 'yuujin':
-     await message.channel.send('gayo pocky ko chak ma')
-
-    elif msg.lower() == 'rocklee':
-     await message.channel.send('gayo samosa dai ko chak ma')
-
-    elif msg.lower() == 'nitrix':
-     await message.channel.send('gayo aryn dai ko chak ma')
-
-    elif msg.lower() == 'akatora':
-     await message.channel.send('gayo mailidd ko chak ma')
-
-    elif msg.lower() == 'sit':
-     await message.channel.send(f'sit on deez nutz {user}')
-
-    elif msg.lower() == 'aryn':
-     await message.channel.send('gayo ako daddy ko ghar ma')
+  
 
      
    
@@ -140,7 +88,7 @@ async def on_message(message):
 
     if msg.lower() == 'gn':
          await message.channel.send(f"GoodNight {message.author.mention}")
-         await message.channel.send("https://tenor.com/view/hila-lund-hilakesoja-muth-muthi-gif-21187080")
+         
 
      
    
